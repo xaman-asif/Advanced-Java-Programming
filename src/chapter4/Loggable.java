e8def1941b429b8a8d3f3fd32fe31a110b214d65
Asif Jaman <asifuzzaman.opi@gmail.com>
2023-12-02 01:59:00 +0600

    Chapter 4: Default Method

---
+++ src/chapter4/Loggable.java
@@ -0,0 +1,25 @@
+package chapter4;
+
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
+
+public interface Loggable {
+  Logger getLogger();
+  String getDelimiter();
+  void log(Level level, String message);
+
+  default void info(String message, Object... objects) {
+    getLogger().log(new LogRecord(Level.SEVERE, formatMessage(message, objects)));
+  }
+
+  // if a default method has too many codes we can extract
+  // them out to a private method
+  private String formatMessage(String message, Object ...args) {
+    StringBuilder sbMessage = new StringBuilder(message);
+
+    for (Object arg : args) {
+
+    }
+  }
+}
